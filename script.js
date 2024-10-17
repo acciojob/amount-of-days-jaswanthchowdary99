a5e063ef093790ef7f000a8806e441a8c4e188e0
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-10-17 15:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+let isLeapYear = ((year % 4 === 0 && year % 100 === 0) || year % 400 === 0);
+	return isLeapYear ? 366 : 365;
+}
+daysOfAYear(2022)
+daysOfAYear(2024)
+daysOfAYear(1900)
+daysOfAYear(2000)
